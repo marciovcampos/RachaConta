31943f5020a4c6f6648edb96a654575f56e23db4
Marcio Vinicius <marcio.campos@outlook.com>
2018-06-25 01:31:37 -0300

    Definição de Layout Criação de Icone Definição de Cores Disposição de botões

--- app/src/main/java/br/com/marcio/rachaconta/MainActivity.java
+++ app/src/main/java/br/com/marcio/rachaconta/MainActivity.java
@@ -1,13 +1,28 @@
 package br.com.marcio.rachaconta;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //DECLARANDO AS PROPRIEDADES VISUAIS
+    private EditText edtValor;
+    private EditText edtNumeroPessoas;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Realizando a refencia aos objetos visuais
+//        edtValor = findViewById(R.id.edtValorId);
+//        edtNumeroPessoas = findViewById(R.id.edtNumeroPessoasId);
+//        txtResultado = findViewById(R.id.txtResultadoId);
+
     }
 }
